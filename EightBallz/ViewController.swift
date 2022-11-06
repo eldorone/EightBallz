67ffb6aa44f3768c1fec386f0d8bea66c5e38d4e
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-11-06 18:50:47 +0900

    Update ViewController.swift

--- EightBallz/ViewController.swift
+++ EightBallz/ViewController.swift
@@ -9,15 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let ballArr: [UIImage] = [UIImage(named: "ball1.png")!,
+    let ballArr: [UIImage] =  [ UIImage(named: "ball1.png")!,
                                 UIImage(named: "ball2.png")!,
                                 UIImage(named: "ball3.png")!,
                                 UIImage(named: "ball4.png")!,
-                                UIImage(named: "ball5.png")!]
+                                UIImage(named: "ball5.png")!
+                              ]
     
     @IBOutlet weak var imageView: UIImageView!
 
-    @IBAction func askButtonPressed(_ sender: UIButton) {
+    @IBAction func askBtnPressed(_ sender: UIButton) {
         
         imageView.image = ballArr[Int.random(in: 0...4)]
     }
